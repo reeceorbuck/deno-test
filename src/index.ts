f285975534572fe1f9aec4d63679b85169af9464
Reece <reeceorbuck@me.com>
2021-08-23 22:26:34 +0800

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+/**
+ * CodeSandbox Template for your Deno Projects
+ * Updated to support Deno 1.0.0
+ *
+ * Reach out to me @dani_akash_ on Twitter if you run into any issues ✌️
+ */
+
+import { serve } from "https://deno.land/std@0.91.0/http/server.ts";
+
+const s = serve({ port: 8080 });
+
+console.log("http://localhost:8080/");
+
+for await (const req of s) {
+  req.respond({ body: "Welcome to Deno!\n" });
+}
